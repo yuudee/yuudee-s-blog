3761bbecd27190f26250428e861ba45ae8aad46b
yuudee <yuudee1018@gmail.com>
2025-03-05 00:03:02 +0900

    タイポ修正

--- my_blog/app/blogs/[slug]/page.tsx
+++ my_blog/app/blogs/[slug]/page.tsx
@@ -45,7 +45,7 @@ const CategoryPage = async ({ params }) => {
                                         {posts.map(post => (
                                             <PostCard
                                                 key={post.slug}
-                                                image={post.image}
+                                                tilte={post.title}
                                                 date={post.date}
                                                 read_time={post.read_time}
                                                 excerpt={post.description}
