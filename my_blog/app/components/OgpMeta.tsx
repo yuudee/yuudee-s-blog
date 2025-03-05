542f83434a364754317fc32a5eefc0307edddecc
yuudee <yuudee1018@gmail.com>
2025-03-05 10:57:48 +0900

    ogpmeta change

--- my_blog/app/components/OgpMeta.tsx
+++ my_blog/app/components/OgpMeta.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Head from "next/head";
 
 const OgpMeta = ({ title, description, image, url }) => {
@@ -9,7 +8,7 @@ const OgpMeta = ({ title, description, image, url }) => {
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
             <meta property="og:url" content={url} />
-            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image" content={image} />
