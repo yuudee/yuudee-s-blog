ad6b3dbc6ef3d680d96572874108548df412b039
yuudee <yuudee1018@gmail.com>
2025-03-05 10:24:39 +0900

    ogpmeta change

--- my_blog/app/components/OgpMeta.tsx
+++ my_blog/app/components/OgpMeta.tsx
@@ -9,7 +9,7 @@ const OgpMeta = ({ title, description, image, url }) => {
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
             <meta property="og:url" content={url} />
-            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:card" content="summary" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image" content={image} />
